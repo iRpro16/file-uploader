e86daf4d5e2ff294e649cc10f59bdd550a0ff88e
iRpro16 <rumeoisidoro16@gmail.com>
2025-04-25 21:30:06 -0400

    chore: initial Prisma setup and project structure

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const path = require("node:path");
+const assetsPath = path.join(__dirname, "/public");
+const session = require("express-session");
+const passport = require("passport");
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
+const { PrismaClient } = require("@prisma/client");
+
+// setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(assetsPath));
+
+// session
+app.use(session({
+    cookie: {
+        maxAge: 7 * 24 * 60 * 60 * 1000 //ms
+    },
+    secret: "pelicans",
+    resave: true,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(
+        new PrismaClient(),
+        {
+            checkPeriod: 2 * 60 * 1000, // ms
+            dbRecordIdIsSessionId: true,
+            dbRecordIdFunction: undefined,
+        }
+    )
+}));
+app.use(passport.session());
+app.use(express.urlencoded( {extended: false }));
+
+app.get("/", (req, res) => {
+    res.send("Hello");
+})
+
+// port
+app.listen(3000, () => {
+    console.log("Listening to on PORT: 3000");
+})
