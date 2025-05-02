f13c376570c870ac5959ce65400d4d0247cf2ec2
iRpro16 <rumeoisidoro16@gmail.com>
2025-05-01 23:28:11 -0400

    refactor: rearrange EJS templates and add minor CSS styling

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,12 +2,12 @@ const fileService = require("../services/fileService");
 const folderService = require("../services/folderService");
 
 async function getRenderIndex(req, res) {
-    const allFiles = req.user ? await fileService.showFiles(req.user.id) : [];
     const allFolders = req.user ? await folderService.showFolders(req.user.id) : [];
+    const allFiles = req.user ? await fileService.showFiles(req.user.id) : [];
     res.render("index", {
         user: req.user,
+        folders: allFolders,
         files: allFiles,
-        folders: allFolders
     })
 }
 
